21179fc4e4dfa03b527858cafaad15c35913e97d
aidar <aidarkhalilov4021@mail.ru>
2022-05-02 13:18:26 +0300

    Ilya 11

--- main.py
+++ main.py
@@ -22,92 +22,93 @@ class BinaryReader():
         self.source = source
 
     def read(self, pattern):
-        pattern = pattern.value
+        pattern = '>' + pattern.value
         result = unpack_from(pattern, self.source, self.offset)
         self.offset += calcsize(pattern)
         return result[0]
 
 
 def a(reader: BinaryReader):
-    a1 = b(reader)
-    a2 = reader.read(Types.int32)
-    a3 = reader.read(Types.uint16)
-    a4 = reader.read(Types.uint64)
-    a5 = reader.read(Types.uint32)
-    a6 = reader.read(Types.int8)
-    a7 = reader.read(Types.int8)
-    return dict(A1=a1, A2=a2, A3=a3, A4=a4, A5=a5, A6=a6, A7=a7)
+    offset = reader.read(Types.uint16)
+    a1 = b(offset, reader.source)
+    a2 = reader.read(Types.int64)
+    a3 = masser_a3(5, reader)
+    a4 = reader.read(Types.uint8)
+    return dict(A1=a1, A2=a2, A3=a3, A4=a4)
 
 
-def b(reader: BinaryReader):
-    b11 = reader.read(Types.uint16)
-    b12 = reader.read(Types.uint32)
-    b1 = masser_b1(b11, b12, reader.source)
-    b21 = reader.read(Types.uint32)
-    b22 = reader.read(Types.uint16)
-    b2 = masser_b2(b21, b22, reader.source)
-    b3 = reader.read(Types.int32)
-    b41 = reader.read(Types.uint32)
-    b4 = d(b41, reader.source)
-    b5 = reader.read(Types.uint16)
-    b6 = reader.read(Types.float)
-    b7 = reader.read(Types.int8)
-    b8 = reader.read(Types.float)
-    return dict(B1=b1, B2=b2, B3=b3, B4=b4, B5=b5, B6=b6, B7=b7, B8=b8)
+def b(offset, source):
+    reader = BinaryReader(offset, source)
+    b1 = masser_b(2, reader)
+    b2 = reader.read(Types.char) + reader.read(Types.char) + reader.read(Types.char) + reader.read(Types.char) + \
+         reader.read(Types.char) + reader.read(Types.char) + reader.read(Types.char) + reader.read(Types.char)
+    b3 = reader.read(Types.double)
+    reader.offset = reader.read(Types.uint32)
+    b4 = d(reader)
+    return dict(B1=b1, B2=b2, B3=b3, B4=b4)
 
 
 def c(reader: BinaryReader):
-    c1 = reader.read(Types.int8)
-    c2 = reader.read(Types.int8)
-    c3 = reader.read(Types.float)
-    c4 = reader.read(Types.float)
-    c51 = reader.read(Types.uint32)
-    c52 = reader.read(Types.uint32)
-    c5 = masser_c5(c51, c52, reader.source)
-    c6 = reader.read(Types.int16)
-    return dict(C1=c1, C2=c2, C3=c3, C4=c4, C5=c5, C6=c6)
+    c1 = reader.read(Types.float)
+    c2 = reader.read(Types.float)
+    return dict(C1=c1, C2=c2)
 
 
-def d(offset, source):
-    reader = BinaryReader(offset, source)
-    d1 = reader.read(Types.int16)
-    d2 = [reader.read(Types.float), reader.read(Types.float), reader.read(Types.float), reader.read(Types.float),
-          reader.read(Types.float), reader.read(Types.float), reader.read(Types.float)]
-    d3 = reader.read(Types.int8)
+def d(reader: BinaryReader):
+    size = reader.read(Types.uint32)
+    offset = reader.read(Types.uint32)
+    d1 = masser_d1(size, offset, reader.source)
+    d2 = reader.read(Types.uint32)
+    d3 = masser_d3(2, reader.offset, reader.source)
     return dict(D1=d1, D2=d2, D3=d3)
 
 
-def masser_b1(size, offset, source):
-    rez = ''
+def masser_a3(size, reader: BinaryReader):
+    offset = reader.offset
+    rez = []
+    while reader.offset < offset + size * 2:
+        rez.append(reader.read(Types.int16))
+    return rez
+
+
+def masser_d1(size, offset, source):
+    reader = BinaryReader(offset, source)
+    rez = []
+    while reader.offset < offset + size * 8:
+        rez.append(reader.read(Types.int64))
+    return rez
+
+
+def masser_d3(size, offset, source):
+    reader = BinaryReader(offset, source)
+    rez = []
+    while reader.offset < offset + size * 2:
+        rez.append(reader.read(Types.uint16))
+    return rez
+
+
+def masser_b(size, reader: BinaryReader):
+    offset = reader.offset
+    rez = []
+    while reader.offset < offset + size * 8:
+        rez.append(c(reader))
+    return rez
+
+
+def masser_c(size, offset, source):
+    rez = ""
     read = BinaryReader(offset, source)
     while read.offset < offset + size:
         rez += str(read.read(Types.char).decode("ascii"))
     return rez
 
 
-def masser_b2(size, offset, source):
-    rez = []
-    read = BinaryReader(offset, source)
-    while read.offset < offset + size * 16:
-        rez.append(c(read))
-    return rez
-
-
-def masser_c5(size, offset, source):
-    rez = []
-    read = BinaryReader(offset, source)
-    while read.offset < offset + size * 8:
-        rez.append(read.read(Types.uint64))
-    return rez
-
-
-source = (b'\xdeYCPT\x02\x008\x00\x00\x00\x02\x00\x00\x00b\x00\x90\xd2\xe8'
-          b'\x1f\x8a\x00\x00\x00\xa0K\nS0\xbfx\x04w\xfe;i\xb6"}\xe8o\xee\xd4'
-          b'-\x01\x01\x80d\x94\x98\x0cp\xb7#\x99apK:e\xe5\xfa\x02\x83\xfc\x7fZO!*\x08'
-          b'\xffc\xa1\xaf/kd;\t*\x95\x02\x83~\xc3:\xbae<\\xlZd\xccR\xb8\x05\x0f\x90t?'
-          b'p_\x82>\x02\x00\x00\x00:\x00\x00\x00=\x01vo\xac\xfa\xed>S\xe1\x00?'
-          b'\x03\x00\x00\x00J\x00\x00\x00r\xe6\x16\xbcF\x85\xab>\x0f\xa5\xe3\xbe\n5*?'
-          b'6\x0bE>\xa8\xd8K?\xfaxq\xbe\x040!?~')
+source = (b'KHTHX\x00Z\xdc\x83>\xf4\xf0V1\x8c\xfbOg\xdfQB\xf3U(_\xca\xe4\x10'
+          b'"\xc9\x0e\xc8+q\x1e_[G\xae\x0e?\xe6\x87\x19\x85}\x86Q\xc1{P^\xa9YN['
+          b'\xfc\xcd\xe4B\xad\xcb\xec\xb6\xc8-\x9faP\x91\\u\x84\x04\x00\x00'
+          b'\x00\x06\x00\x00\x00\x1a\xa3\xe7\x08\xd1YY\xf1\xd8\xba\xae2a\xbe\xbb'
+          b'\xab"\xbf"\x15\x94\xbe\x82\xe8\xf3jcdvdpwi?\x9a\xbe\xc1\xc4}\x89\x00\x00\x00'
+          b'\x00J')
 
 reader = BinaryReader(offset=5, source=source)
 g = a(reader)
