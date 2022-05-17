7b058ec58627be6e405bc23f47e6961e2944f7d6
aidar <aidarkhalilov4021@mail.ru>
2022-05-17 14:40:59 +0300

    Third

--- mirea/practice/third.py
+++ mirea/practice/third.py
@@ -1,4 +1,12 @@
-from sys import modules
+import sys
+from importlib import reload
+import csv
+import math
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import random as rand
+from distutils.core import setup
 
 # Задача 1. Приведите примеры кода, которые соответствуют нарушениям PEP 8:
 # 1. вызов функции foo ()
@@ -12,10 +20,119 @@ from sys import modules
 # 9. if something == True
 
 
-# Задача 2. Как вы думаете, модуль загружается один раз или загружается каждый раз при очередном импорте?
+# Задача 2. Как вы думаете, модуль загружается один раз или загружается
+# каждый раз при очередном импорте?
 # Докажите правильность вашей гипотезы кодом.
-# Модули Python не импортируются несколько раз, поэтому повторное выполнение команды оператора импорта не приведет к
-# перезагрузке модуля. Если вы хотите, чтобы он был перезагружен, вам необходимо выполнить инструкцию reload.
+# Модули Python не импортируются несколько раз, поэтому повторное выполнение
+# команды оператора импорта не приведет к перезагрузке модуля. Если вы хотите,
+# чтобы он был перезагружен, вам необходимо выполнить инструкцию reload.
+# print(sys.modules)
+math = reload(math)
 
 
 # Задача 3. GLOBAL VARIABLE относится только к одному пользователю
+
+# Задача 6. Напишите функцию, которая добавляет информацию о возникшем
+# исключении (класс, сообщение, трассировка) в лог-файл.
+def delete(z):
+    try:
+        x = math.log(z)
+        print(int("f"))
+        print(x)
+    except (ZeroDivisionError, ValueError) as e:
+        return run_with_log(e)
+
+
+def run_with_log(func):
+    with open('error_log.txt', 'w', -1, 'utf-8') as f:
+        f.write(f"{str(func)} {sys.exc_info()} - your error")
+
+
+# delete(-1) - выражение под логарифмом отрицательное.
+# Задача 7. Реализуйте процедурную генерацию спрайтов 5x5 пикселей
+# с помощью Matplotlib и функции imshow.
+def square_gen():
+    square = [[0] * 5,
+              [0] * 5,
+              [0] * 5,
+              [0] * 5,
+              [0] * 5]
+    for i in range(5):
+        for j in range(3):
+            square[i][j] = rand.randint(0, 1)
+            if square[i][j] == 1 and j != 2:
+                square[i][4 - j] = 1
+    return square
+
+
+def square_show(square):
+    res = np.zeros((5, 5, 3))
+    for i in range(5):
+        for j in range(5):
+            if square[i][j] == 1:
+                res[i][j] = [255, 255, 255]
+    return res
+
+
+def main():
+    square_show(square_gen())
+    fig, axes = plt.subplots(nrows=20, ncols=20)
+    for ax in axes.flat:
+        ax.imshow(square_show(square_gen()))
+        ax.axis('off')
+    plt.show()
+
+
+main()
+
+
+# Задача 11. Проанализируйте базу данных старых компьютерных игр.
+def games():
+    j = 0
+    years = []
+    listing = []
+    with open('GAMES.csv', encoding='utf-8') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            z = "".join(row)
+            listing.append(z)
+    mes = listing
+    for i in mes:
+        temple_str = ''.join(i).split(';')
+        if temple_str[3].strip('"') != "не издана":
+            if int(temple_str[3].strip('"')) not in years:
+                years.append(int(temple_str[3].strip('"')))
+    popular = [0] * len(years)
+    for i in mes:
+        temple_str = ''.join(i).split(';')
+        if temple_str[3].strip('"') != "не издана":
+            if years[j] == int(temple_str[3].strip('"')):
+                popular[j] += 1
+            else:
+                j += 1
+                popular[j] += 1
+    plt.bar([str(i) for i in years], popular)
+    plt.title('Распределение игр по популярности выхода')
+    plt.show()
+    themes = []
+    for i in mes:
+        temple_str = ''.join(i).split(';')
+        if temple_str[1].strip('"') not in themes:
+            themes.append(temple_str[1].strip('"'))
+    data = {i: [0] * len(years) for i in sorted(themes)}
+    print(data)
+    for i in mes:
+        temple_str = ''.join(i).split(';')
+        if temple_str[3].strip('"') != "не издана":
+            data[temple_str[1].strip('"')][
+                years.index(int(temple_str[3].strip('"')))] += 1
+    print(years)
+    pd.DataFrame(data)
+    for k, v in data.items():
+        plt.plot(years, v, label=k)
+    plt.legend(data, loc=2)
+    plt.title('Популярные жанры в различные периоды времени')
+    plt.show()
+
+
+# games()
